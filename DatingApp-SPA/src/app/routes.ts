0375aed9ce1ad08b0903bcf4332e74aeb95f9a49
VTC <vtchris.2357@gmail.com>
2020-09-24 14:18:26 -0400

    Basic routing 64

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,13 @@
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'members', component: MemberListComponent },
+    { path: 'messages', component: MessagesComponent },
+    { path: 'lists', component: ListsComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
