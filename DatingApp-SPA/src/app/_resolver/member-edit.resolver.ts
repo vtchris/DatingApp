40c51d3e1e7433222da2f24b54b0510f5306378f
VTC <vtchris.2357@gmail.com>
2020-10-01 14:27:17 -0400

    357: User Edit Page

---
+++ DatingApp-SPA/src/app/_resolver/member-edit.resolver.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './../_services/auth.service';
+import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { AlertifyService } from '../_services/alertify.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+
+@Injectable()
+export class MemberEditResolver implements Resolve<User> {
+    constructor(
+        private userService: UserService,
+        private router: Router,
+        private alertify: AlertifyService,
+        private authService: AuthService) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
+            catchError(error => {
+                this.alertify.error('Problem retrieving data');
+                this.router.navigate(['/members']);
+                return of(null);
+            })
+        );
+    }
+}
